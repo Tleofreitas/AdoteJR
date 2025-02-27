d28b7743e54f339e37d11cdbc284019b544385b5
Thiago Freitas <thiagoleonardo1995@live.com>
2025-02-27 00:29:07 -0300

    ViewBinding, Tela de Cadastro inicial com Toobar customizada

--- app/src/main/java/com/example/appadotejrtlrf/CadastroActivity.kt
+++ app/src/main/java/com/example/appadotejrtlrf/CadastroActivity.kt
@@ -1,40 +1,48 @@
 package com.example.appadotejrtlrf
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.appadotejrtlrf.databinding.ActivityLoginBinding
-import com.example.appadotejrtlrf.databinding.ActivityMainBinding
+import com.example.appadotejrtlrf.databinding.ActivityCadastroBinding
 
-class LoginActivity : AppCompatActivity() {
+class CadastroActivity : AppCompatActivity() {
 
     private val binding by lazy {
-        ActivityLoginBinding.inflate(layoutInflater)
+        ActivityCadastroBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // enableEdgeToEdge()
         setContentView(binding.root)
-
-        inicializarEventosClique()
         /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        */
+         */
+
+        incializarToolbar()
     }
 
-    private fun inicializarEventosClique() {
-        binding.textCadastro.setOnClickListener {
-            startActivity(
-                Intent(this, CadastroActivity::class.java)
-            )
+    /*private fun inicializarToolbar() {
+        val toolbar = binding.includeToolbar.tbPrincipal
+        setSupportActionBar( toolbar )
+        supportActionBar?.apply {
+            title = "Faça seu Cadastro"
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }*/
+
+    private fun incializarToolbar() {
+        val toolbar = binding.includeToolbar.tbPrincipal
+        setSupportActionBar( toolbar )
+        supportActionBar?.apply {
+            title = "Cadastro de Usuário"
+            setDisplayHomeAsUpEnabled(true)
         }
     }
 }
