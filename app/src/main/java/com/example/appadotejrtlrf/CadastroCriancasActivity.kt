7b587311bb1015d826811e270363adf23c50a6a1
Thiago Freitas <thiagoleonardo1995@live.com>
2025-02-28 21:51:32 -0300

    Tela inicial

---
+++ app/src/main/java/com/example/appadotejrtlrf/CadastroCriancasActivity.kt
@@ -0,0 +1,39 @@
+package com.example.appadotejrtlrf
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.appadotejrtlrf.databinding.ActivityCadastroCriancasBinding
+import com.example.appadotejrtlrf.databinding.ActivityLoginBinding
+
+class CadastroCriancasActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityCadastroCriancasBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // enableEdgeToEdge()
+        setContentView(binding.root)
+        /*
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+         */
+        incializarToolbar()
+    }
+
+    private fun incializarToolbar() {
+        val toolbar = binding.includeToolbar.tbPrincipal
+        setSupportActionBar( toolbar )
+        supportActionBar?.apply {
+            title = "Cadastro de criança"
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+}
