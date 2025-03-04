8534de724d12836573d18f497c5c83d8b6d6cb67
Thiago Freitas <thiagoleonardo1995@live.com>
2025-03-04 00:54:26 -0300

    Toolbar inferior geral iniciada

--- app/src/main/java/com/example/adotejr/CadastroCriancasActivity.kt
+++ app/src/main/java/com/example/adotejr/CadastroCriancasActivity.kt
@@ -1,14 +1,22 @@
 package com.example.adotejr
 
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.forEach
 import com.example.adotejr.databinding.ActivityCadastroCriancasBinding
+import com.google.android.material.appbar.MaterialToolbar
 
 class CadastroCriancasActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityCadastroCriancasBinding.inflate(layoutInflater)
     }
+
+    private lateinit var imgPesquisar: ImageView
+    private lateinit var imgCadastrar: ImageView
+    private lateinit var imgPerfil: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +30,22 @@ class CadastroCriancasActivity : AppCompatActivity() {
         }
          */
         incializarToolbar()
+
+        imgPesquisar = binding.includeToolbarInferior.tbPesquisar
+        imgCadastrar = binding.includeToolbarInferior.tbCadastrar
+        imgPerfil = binding.includeToolbarInferior.tbPerfil
+
+        val icons = listOf(imgPesquisar, imgCadastrar, imgPerfil)
+        imgCadastrar.isSelected = true
+
+        icons.forEach { icon ->
+            icon.setOnClickListener {
+                // Redefine o estado selecionado para todos os ícones
+                icons.forEach { it.isSelected = false }
+                // Define o estado selecionado para o ícone clicado
+                icon.isSelected = true
+            }
+        }
     }
 
     private fun incializarToolbar() {
