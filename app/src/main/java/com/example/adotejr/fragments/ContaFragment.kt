c44a5747beaeb89d981222c97abe17ec0408e1ab
Thiago Freitas <thiagoleonardo1995@live.com>
2025-03-08 23:16:32 -0300

    Selecionar imagem para perfil iniciado

--- app/src/main/java/com/example/adotejr/fragments/ContaFragment.kt
+++ app/src/main/java/com/example/adotejr/fragments/ContaFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.adotejr.databinding.FragmentContaBinding
 
@@ -25,5 +27,34 @@ class ContaFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.fabSelecionar.setOnClickListener {
+            mostrarDialogoEscolherImagem()
+        }
+    }
+
+    private fun mostrarDialogoEscolherImagem() {
+        val view = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_imagem, null)
+        val dialog = android.app.AlertDialog.Builder(requireContext())
+            .setView(view)
+            .create()
+
+        view.findViewById<Button>(R.id.button_camera).setOnClickListener {
+            // abrirCamera()
+            Toast.makeText(requireContext(), "CAMERA", Toast.LENGTH_LONG).show()
+            dialog.dismiss()
+        }
+
+        view.findViewById<Button>(R.id.button_gallery).setOnClickListener {
+            // abrirGaleria()
+            Toast.makeText(requireContext(), "GALERIA", Toast.LENGTH_LONG).show()
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 }
