92d43dfe8cfc6c06f63aa0bfff4b2abbd3e2c849
Thiago Freitas <thiagoleonardo1995@live.com>
2025-05-14 22:17:34 -0300

    Baixar Cartões configurado

--- app/src/main/java/com/example/adotejr/fragments/ReportsFragment.kt
+++ app/src/main/java/com/example/adotejr/fragments/ReportsFragment.kt
@@ -1,26 +1,31 @@
 package com.example.adotejr.fragments
 
-import android.app.Activity.RESULT_OK
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.documentfile.provider.DocumentFile
 import androidx.fragment.app.Fragment
 import com.example.adotejr.databinding.FragmentReportsBinding
 import com.example.adotejr.utils.ExportadorCadastros
 import com.example.adotejr.utils.ExportadorUsuarios
 import com.example.adotejr.utils.NetworkUtils
-import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.Firebase
+import com.google.firebase.storage.storage
+import java.io.File
 import java.time.LocalDate
 
 class ReportsFragment : Fragment() {
     private lateinit var binding: FragmentReportsBinding
     private var callbackExcel: ((Uri) -> Unit)? = null // Variável para armazenar o callback
     private var ano = LocalDate.now().year
-    private val REQUEST_FOLDER = 1001
+    val REQUEST_FOLDER = 1001
+    val CREATE_DOCUMENT_REQUEST_CODE = 1002
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,7 +38,6 @@ class ReportsFragment : Fragment() {
 
         binding.btnBaixarUsuarios.setOnClickListener {
             if (NetworkUtils.conectadoInternet(requireContext())) {
-                // solicitarLocalParaSalvarExcel()
                 solicitarLocalParaSalvarExcel { uri ->
                     ExportadorUsuarios(requireContext()).exportarComDados(uri)
                 }
@@ -52,16 +56,20 @@ class ReportsFragment : Fragment() {
             }
         }
 
-        /*
         binding.btnBaixarCartoes.setOnClickListener {
             if (NetworkUtils.conectadoInternet(requireContext())) {
-                val caminho = "cartoes/$ano"
-                // Configurar
+                selecionarPasta()
             } else {
                 Toast.makeText(requireContext(), "Verifique a conexão com a internet e tente novamente!", Toast.LENGTH_LONG).show()
             }
-        } */
+        }
         return binding.root
+    }
+
+    // Método para abrir o seletor de pasta
+    private fun selecionarPasta() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+        startActivityForResult(intent, REQUEST_FOLDER)
     }
 
     private fun solicitarLocalParaSalvarExcel(callback: (Uri) -> Unit) {
@@ -76,10 +84,102 @@ class ReportsFragment : Fragment() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == CREATE_DOCUMENT_REQUEST_CODE && resultCode == RESULT_OK) {
-            data?.data?.let { uri ->
-                callbackExcel?.invoke(uri) // Chama o callback armazenado
+
+        when (requestCode) {
+            REQUEST_FOLDER -> { // Tratamento para seleção de pasta
+                if (resultCode == Activity.RESULT_OK) {
+                    data?.data?.let { uri ->
+                        baixarCartoes(uri)
+                    }
+                }
             }
+
+            CREATE_DOCUMENT_REQUEST_CODE -> { // Tratamento para download do arquivo Excel
+                if (resultCode == Activity.RESULT_OK) {
+                    data?.data?.let { uri ->
+                        callbackExcel?.invoke(uri) // Chama o callback armazenado
+                    }
+                }
+            }
+        }
+    }
+
+    // Método para buscar e baixar os arquivos do Firebase Storage
+    private fun baixarCartoes(pastaDestinoUri: Uri) {
+        // Exibir a ProgressBar enquanto os arquivos estão sendo baixados
+        binding.textBaixando.visibility = View.VISIBLE
+        binding.progressBar.visibility = View.VISIBLE
+
+        // Referência à pasta no Firebase Storage
+        val storageRef = Firebase.storage.reference.child("cartoes/$ano/")
+
+        // Listar todos os arquivos dentro da pasta "cartoes/2025/"
+        storageRef.listAll().addOnSuccessListener { listResult ->
+            val arquivos = listResult.items  // Lista de arquivos encontrados
+            val totalArquivos = arquivos.size  // Número total de arquivos
+
+            // Se não houver arquivos na pasta, exibe uma mensagem e interrompe o processo
+            if (totalArquivos == 0) {
+                binding.textBaixando.visibility = View.GONE
+                binding.progressBar.visibility = View.GONE
+                Toast.makeText(requireContext(), "Nenhum arquivo encontrado para download!", Toast.LENGTH_LONG).show()
+                return@addOnSuccessListener  // Sai do método sem executar downloads
+            }
+
+            var arquivosBaixados = 0  // Contador para acompanhar os arquivos baixados
+            val pickedDir = DocumentFile.fromTreeUri(requireContext(), pastaDestinoUri) // Obtém o diretório escolhido pelo usuário
+
+            // Verifica se o diretório de destino é válido
+            if (pickedDir == null || !pickedDir.isDirectory) {
+                Log.e("Download", "Diretório inválido!")
+                binding.textBaixando.visibility = View.GONE
+                binding.progressBar.visibility = View.GONE
+                Toast.makeText(requireContext(), "Erro ao acessar a pasta de destino", Toast.LENGTH_LONG).show()
+                return@addOnSuccessListener // Sai do método
+            }
+
+            // Itera sobre cada arquivo listado no Firebase Storage
+            arquivos.forEach { fileRef ->
+                val tempFile = File(requireContext().cacheDir, fileRef.name)  // Cria um arquivo temporário para o download
+
+                // Faz o download do arquivo temporário
+                fileRef.getFile(tempFile).addOnSuccessListener {
+                    // Cria um novo arquivo dentro da pasta escolhida pelo usuário
+                    val newFile = pickedDir.createFile("application/pdf", fileRef.name)
+
+                    // Se o arquivo foi criado corretamente, copia os dados do arquivo temporário para ele
+                    newFile?.uri?.let { uri ->
+                        requireContext().contentResolver.openOutputStream(uri)?.use { outputStream ->
+                            tempFile.inputStream().use { inputStream ->
+                                inputStream.copyTo(outputStream) // Copia os dados do arquivo temporário para o destino final
+                            }
+                        }
+                        tempFile.delete() // Remove o arquivo temporário após a cópia
+                    }
+
+                    arquivosBaixados++  // Incrementa o contador de arquivos baixados
+                    atualizarProgresso(arquivosBaixados, totalArquivos)  // Atualiza a barra de progresso
+
+                }.addOnFailureListener { error ->
+                    Toast.makeText(requireContext(), "Erro ao baixar ${fileRef.name}", Toast.LENGTH_LONG).show()
+                    Log.e("Download", "Erro ao baixar ${fileRef.name}: ${error.message}") // Loga erros caso o download falhe
+                }
+            }
+        }.addOnFailureListener { error ->
+            Toast.makeText(requireContext(), "Erro ao listar arquivos", Toast.LENGTH_LONG).show()
+            Log.e("Firebase", "Erro ao listar arquivos: ${error.message}")
+            binding.textBaixando.visibility = View.GONE
+            binding.progressBar.visibility = View.GONE // Esconde a barra de progresso caso ocorra um erro na listagem
+        }
+    }
+
+    // Método para atualizar o progresso do download
+    private fun atualizarProgresso(atual: Int, total: Int) {
+        binding.progressBar.progress = (atual * 100) / total
+        if (atual == total) {
+            binding.textBaixando.visibility = View.GONE
+            binding.progressBar.visibility = View.GONE
+            Toast.makeText(requireContext(), "Download concluído!", Toast.LENGTH_LONG).show()
         }
     }
 
