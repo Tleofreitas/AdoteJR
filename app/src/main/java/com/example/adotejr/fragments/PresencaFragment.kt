01ca6a332ed895e7b7226dd7c0cfd2f75a2ecc6c
Thiago Freitas <thiagoleonardo1995@live.com>
2025-11-18 14:00:41 -0300

    Abas em Listagem 2) PresencaFragment

---
+++ app/src/main/java/com/example/adotejr/fragments/PresencaFragment.kt
@@ -0,0 +1,19 @@
+package com.example.adotejr.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.adotejr.R
+
+class PresencaFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Infla o layout para este fragmento
+        return inflater.inflate(R.layout.fragment_presenca, container, false)
+    }
+}
